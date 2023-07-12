128b4adbdf07c17d5b08cf383d7cbb7f08903576
rickyrojas1 <rickyrojas@me.com>
2023-07-11 23:36:05 -0400

    update middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -14,8 +14,15 @@ module.exports = [
       },
     },
   },
-  'strapi::cors',
   'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:1337', 'http://example2'],
+    },
+  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
