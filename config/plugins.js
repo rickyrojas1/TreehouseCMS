e2d04416b3e9c1bb14296f3476e5d2d7ad481ae4
rickyrojas1 <rickyrojas@me.com>
2024-07-25 20:36:38 -0400

    product db init

--- config/plugins.js
+++ config/plugins.js
@@ -27,6 +27,18 @@ module.exports = ({ env }) => ({
         },
       },
     },
+    email: {
+      config: {
+        provider: 'strapi-provider-email-resend',
+        providerOptions: {
+          apiKey: env('RESEND_API_KEY'), // Required
+        },
+        settings: {
+          defaultFrom: 'info@treehousecannabis.com',
+          defaultReplyTo: 'info@treehousecannabis.com',
+        },
+      }
+    },
     'content-source-map': {
       enabled: true,
       config: {
